83a579be24e12d5aeaf873e4c06afb29298bd281
winstonlopez <ev.winston2304@gmail.com>
2022-11-25 16:44:10 +0800

    fixing mobile responsiveness

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ for(let i = 0; i < list.length; i++){
     span.append(list[i]);
 }
 
+//create event delegation to show/hide small menu
+
+document.addEventListener('click', function(event){
+    let id = event.target.closest('div').dataset.toggleId;
+    if(!id){
+         //if there is no dataset with toggleId attribute, check if there is a hidden attribute
+         let menu = document.getElementById("small-screen-menu");
+            if(!menu.hidden){
+                menu.hidden = true;
+            }
+         return; 
+        }
+    let elem = document.getElementById(id);
+    elem.hidden = !elem.hidden;
+  
+
+})
